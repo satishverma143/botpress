4639a7f21eabdd45cfb288dda86e159b741a66ae
Eugene Mirotin <emirotin@gmail.com>
2018-05-24 16:46:48 +0500

    feat(qna): redirect to node middleware implementation, not working yet

--- packages/functionals/botpress-qna/src/middleware.js
+++ packages/functionals/botpress-qna/src/middleware.js
@@ -1,7 +1,9 @@
 import { NLU_PREFIX } from './storage'
 
-export const processEvent = async (event, { storage, logger }) => {
-  if (!event.nlu.intent || !event.nlu.intent.startsWith(NLU_PREFIX)) {
+export const processEvent = async (event, { bp, storage, logger }) => {
+  // NB: we rely on NLU being loaded before we receive any event.
+  // I'm not sure yet if we can guarantee it
+  if (!event.nlu || !event.nlu.intent || !event.nlu.intent.startsWith(NLU_PREFIX)) {
     return false
   }
 
@@ -21,6 +23,13 @@ export const processEvent = async (event, { storage, logger }) => {
     // return `true` to prevent further middlewares from capturing the message
     return true
   } else if (data.action === 'redirect') {
-    logger.debug('QnA: replying to recognized question with redirect: UNSUPPORTED YET', id)
+    logger.debug('QnA: replying to recognized question with redirect', id)
+    // TODO: This is used as the `stateId` by the bot template
+    // Not sure if it's universal enough for every use-case but
+    // I don't see a better alternative as of now
+    const stateId = event.sessionId || event.user.id
+    await bp.dialogEngine.jumpTo(stateId, data.redirectFlow, data.redirectNode)
+    bp.dialogEngine.processMessage(stateId, event)
+    return true
   }
 }
